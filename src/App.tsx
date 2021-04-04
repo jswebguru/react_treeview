ddfd9c1a2338684b8546819130273e761fe45426
jswebguru <iotfan90@gmail.com>
2021-04-04 12:18:36 +0200

    Done

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Preview from './views/preview';
+
+function App() {
+  return (
+    <div className="App">
+      <Preview />
+    </div>
+  );
+}
+
+export default App;
